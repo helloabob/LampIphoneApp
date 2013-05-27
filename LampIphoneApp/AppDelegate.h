4cdd72bbbc43a7ed8eea57f2ba255b8a3dedb876
wangbo <wangbo@zhangdeiMac.local>
2013-05-27 12:26:11 +0800

    Initial Commit

---
+++ LampIphoneApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LampIphoneApp
+//
+//  Created by wangbo on 13-5-27.
+//  Copyright (c) 2013年 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LampIphoneApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LampIphoneApp
+//
+//  Created by wangbo on 13-5-27.
+//  Copyright (c) 2013年 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
