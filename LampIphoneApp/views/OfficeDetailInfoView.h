375cf2abc1ab9e32b57fe431276b630da65e8be2
helloabob <helloabob@gmail.com>
2013-06-07 11:40:18 +0800

    update

---
+++ LampIphoneApp/views/OfficeDetailInfoView.h
@@ -0,0 +1,13 @@
+//
+//  OfficeDetailInfoView.h
+//  LampIphoneApp
+//
+//  Created by wangbo on 13-6-6.
+//  Copyright (c) 2013年 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfficeDetailInfoView : UIView
+
+@end
---
+++ LampIphoneApp/views/OfficeDetailInfoView.m
@@ -0,0 +1,31 @@
+//
+//  OfficeDetailInfoView.m
+//  LampIphoneApp
+//
+//  Created by wangbo on 13-6-6.
+//  Copyright (c) 2013年 wangbo. All rights reserved.
+//
+
+#import "OfficeDetailInfoView.h"
+
+@implementation OfficeDetailInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
